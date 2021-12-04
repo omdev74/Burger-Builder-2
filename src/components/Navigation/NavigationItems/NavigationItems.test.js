723dbbb0db155e10107fc11cb034364385b4f6d5
Om Dev <omdev74@gmail.com>
2021-12-05 04:09:52 +0530

    0.20.6 beforeEach()

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,9 +7,16 @@ import NavigationItem from "./NavigationItem/NavigationItem"
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallow(<NavigationItems />)
+    })
     it("should render 2 <NavigationItems /> elem if not authenticated",()=>{
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     })
-    
+    it("should render 3 <NavigationItems /> elem if authenticated",()=>{
+        // wrapper = shallow(<NavigationItems isAuthenticated/>);
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    })
 })
